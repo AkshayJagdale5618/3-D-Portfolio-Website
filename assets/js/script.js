dc91902876c97c88f872b0d078aefbd4b9f9c863
Akshay <akshay7777j@gmail.com>
2024-03-24 11:27:19 +0530

    Page 1-2-3-4-5 Done

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Turn Pages When Click Next Or Prev Button
+
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+ 
+pageTurnBtn.forEach((el, index) => {
+    el.onclick = () => {
+        const pageTurnId = el.getAttribute('data-page');
+        const pageTurn = document.getElementById(pageTurnId);
+
+        if (pageTurn.classList.contains('turn')) {
+            pageTurn.classList.remove('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 - index;
+            }, 500)
+        }
+        else {
+            pageTurn.classList.add('turn');
+            setTimeout(() => {
+                pageTurn.style.zIndex = 20 + index;
+            }, 500)
+        }
+    }
+})
