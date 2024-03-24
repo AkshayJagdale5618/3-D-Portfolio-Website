3636bf31394f33360aeeb711a6e79e3baf82ccb0
Akshay <akshay7777j@gmail.com>
2024-03-24 15:35:01 +0530

    Project Completed  Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // Turn Pages When Click Next Or Prev Button
-
 const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
  
 pageTurnBtn.forEach((el, index) => {
@@ -20,4 +19,82 @@ pageTurnBtn.forEach((el, index) => {
             }, 500)
         }
     }
+})
+
+// Contact Me button when click
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+    pages.forEach((page, index) => {
+        setTimeout(() => {
+         page.classList.add('turn');
+
+         setTimeout(() => {
+            page.style.zIndex = 20 + index;
+         }, 500)
+        }, (index + 1) * 200 + 100)
+    })
+}
+
+
+// Create reverse index function
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+    pageNumber--;
+    if (pageNumber <0) {
+        pageNumber = totalPages -1;
+    }
+}
+
+//Back Profile button when click
+const backProfileBtn =document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+    pages.forEach((_,index) => {
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+            
+            setTimeout(() => {
+                reverseIndex();
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+
+        }, (index + 1) * 200 + 100 )
+    })
+}
+
+// Opening Animation
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
+
+// Opening Animation ( Cover Right Animation )
+setTimeout(() => {
+    coverRight.classList.add('turn');
+}, 2100)
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1;
+}, 2800)
+
+// Opening Animation ( page Left or Profile Animation )
+setTimeout(() => {
+    pageLeft.style.zIndex = 20;
+}, 3200)
+
+// Opening Animation ( All page Right Animation )
+pages.forEach((_,index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+        
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].style.zIndex = 10 + index;
+        }, 500)
+
+    }, (index + 1) * 200 + 2100 )
 })
